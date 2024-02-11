fb088260aa9d1ffe5b7bbe494e47554170235fbc
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2024-02-11 21:54:45 +0200

    sign up page

---
+++ ka_laba/lib/signup.dart
@@ -0,0 +1,133 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:ka_laba/menu.dart';
+import 'package:ka_laba/welcome-login-page.dart';
+
+// LOGIN PAGE
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(
+              alignment: Alignment.center,
+              height: 95,
+              width: 272,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black, width: 1.0),
+              ),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(
+                      'Kalaba',
+                      style: TextStyle(fontSize: 40),
+                    ),
+                    Text("Sign Up")
+                  ]),
+            ),
+            Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(40),
+              height: 377,
+              width: 272,
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 1.0)),
+              // ignore: prefer_const_constructors
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text('name?'),
+                    SizedBox(
+                      height: 39,
+                      width: 161,
+                      child: TextField(
+                        decoration:
+                            InputDecoration(border: OutlineInputBorder()),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text('secret word?'),
+                    SizedBox(
+                      height: 39,
+                      width: 161,
+                      child: TextField(
+                        decoration:
+                            InputDecoration(border: OutlineInputBorder()),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text('repeat word'),
+                    SizedBox(
+                      height: 42,
+                      width: 161,
+                      child: TextField(
+                        decoration:
+                            InputDecoration(border: OutlineInputBorder()),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => MenuPage()));
+                      },
+                      style: TextButton.styleFrom(
+                        padding: EdgeInsets.zero,
+                        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                        backgroundColor: Colors.transparent,
+                        elevation: 0,
+                      ),
+                      child: Text(
+                        'sign up',
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          color: Colors.black,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => LoginPage()));
+                        },
+                        style: TextButton.styleFrom(
+                          padding: EdgeInsets.zero,
+                          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                          backgroundColor: Colors.transparent,
+                          elevation: 0,
+                        ),
+                        child: Text(
+                          'login instead',
+                          style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              color: Colors.blue),
+                        ))
+                  ]),
+            )
+          ],
+        ),
+      )),
+    );
+  }
+}
